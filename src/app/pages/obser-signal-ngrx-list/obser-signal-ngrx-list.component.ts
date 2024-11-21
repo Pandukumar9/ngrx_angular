b16a51f28a2161b7757ffb4d6ec81658c14d1b48
Pandukumar9 <pandukumar.gujja@gmail.com>
2024-11-21 23:59:01 +0530

    removed comments

--- src/app/pages/obser-signal-ngrx-list/obser-signal-ngrx-list.component.ts
+++ src/app/pages/obser-signal-ngrx-list/obser-signal-ngrx-list.component.ts
@@ -1,17 +1,15 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { Component, OnInit } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ApiService } from 'src/app/services/api.service';
 import { ReuseDialogComponent } from '../reuse-dialog/reuse-dialog.component';
-
 @Component({
   selector: 'obser-signal-ngrx-list',
   templateUrl: './obser-signal-ngrx-list.component.html',
   styleUrls: ['./obser-signal-ngrx-list.component.scss']
 })
-export class ObserSignalNgrxListComponent {
-  constructor(private signal:ApiService,private fb: FormBuilder,private modalService: BsModalService,private store: Store,private cdr: ChangeDetectorRef){}
+export class ObserSignalNgrxListComponent implements OnInit{
+
+  constructor(private signal:ApiService,private modalService: BsModalService){}
 
   ngOnInit() {
     this.signal.loadData();
@@ -24,11 +22,6 @@ export class ObserSignalNgrxListComponent {
   bsModalRef!: BsModalRef;
 
   addUser(){
-    // const initialState: Partial<ReuseDialogComponent> = {
-    //   // title: 'Add User',
-    //   userData: { name: 'Alice', email: 'alice@example.com',mobile :'7432434223' }, // Data to pass
-    // };
-
     this.bsModalRef = this.modalService.show(ReuseDialogComponent, {
       // initialState, // Pass the initialState object
       animated: true,
@@ -36,47 +29,37 @@ export class ObserSignalNgrxListComponent {
       class: 'modal-md',
     });
 
-       // Listen for the `onHidden` event
-       this.bsModalRef.onHidden?.subscribe(() => {
-        this.signal.loadData();// Call getUsers method when the modal is closed
-      });
+    // Listen for the `onHidden` event
+    this.bsModalRef.onHidden?.subscribe(() => {
+      this.signal.loadData();// Call getUsers method when the modal is closed
+    });
   }
 
   onDelete(id:number){
     if (confirm('Are you sure you want to delete this user?')) {
-      // Dispatch the delete action
       this.signal.deleteData(id);
     }
   }
 
   onEdit(edituser:any){
-    // Edit user
-    // this.signal.updateData(edituser);
-    // this.userToEdit = null;
-
-    // const initialState: Partial<ReuseDialogComponent> = {
-    //   // title: 'Add User',
-    //   userData: { name: 'Alice', email: 'alice@example.com',mobile :'7432434223' }, // Data to pass
-    // };
     this.bsModalRef = this.modalService.show(ReuseDialogComponent,
     {
       initialState: {
         userdata: edituser, // Pass the data here
       },
-      // initialState, // Pass the initialState object
       animated: true,
       // backdrop: 'static',
       class: 'modal-md',
     },
   );
 
-       // Listen for the `onHidden` event
-       this.bsModalRef.onHidden?.subscribe(() => {
-        this.signal.loadData();// Call getUsers method when the modal is closed
-      });
+  // Listen for the `onHidden` event
+  this.bsModalRef.onHidden?.subscribe(() => {
+    this.signal.loadData();// Call getUsers method when the modal is closed
+  });
 }
-onFetchData() {
+  onFetchData() {
   // Dispatch load API data with a specific URL for users
-  this.signal.loadData();
-}
+    this.signal.loadData();
+  }
 }
